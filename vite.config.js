b88b7078e597a4f43ef846d604328de1c3c920cd
anorak097 <anorakyang@gmail.com>
2026-06-22 16:50:47 +0800

    調整 GitHub Pages 部署工作流程中的建置指令，移除 base 參數以簡化建置過程；更新 vite.config.js 檔案的 base 設定為 '/prolouge/' 以符合專案需求

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,26 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: '',
-    server: {
-        fs: {
-            strict: false
-        }
-    },
-    resolve: {
-        alias: {
-            path: 'path-browserify',
-            fs: resolve(__dirname, './fs-stub.ts')
-        }
-    },
-    define: {
-        __dirname: '""',
-        'global': {},
-        'process.cwd': '() => "/"',
-        'process.env': '{}'
-    },
-    // @ts-ignore
-    slidev: {
-        routerMode: 'hash',
-    }
+	base: '/prolouge/',
+	server: {
+		fs: {
+			strict: false
+		}
+	},
+	resolve: {
+		alias: {
+			path: 'path-browserify',
+			fs: resolve(__dirname, './fs-stub.ts')
+		}
+	},
+	define: {
+		__dirname: '""',
+		'global': {},
+		'process.cwd': '() => "/"',
+		'process.env': '{}'
+	},
+	// @ts-ignore
+	slidev: {
+		routerMode: 'hash',
+	}
 })
